49c123e5623b2c8320223560291dcc683ec9f9bc
andytyc <andy.tengyc@gmail.com>
2022-04-07 11:39:19 +0800

    upated hystrix rolling

--- hystrix/rolling/rolling.go
+++ hystrix/rolling/rolling.go
@@ -7,6 +7,9 @@ import (
 
 // Number tracks a numberBucket over a bounded number of
 // time buckets. Currently the buckets are one second long and only the last 10 seconds are kept.
+//
+// Number 在一定数量的时间桶上跟踪 numberBucket。
+// 目前，存储桶的长度为一秒，仅保留最后 10 秒。
 type Number struct {
 	Buckets map[int64]*numberBucket
 	Mutex   *sync.RWMutex
@@ -17,6 +20,8 @@ type numberBucket struct {
 }
 
 // NewNumber initializes a RollingNumber struct.
+//
+// NewNumber 初始化一个 RollingNumber 结构。
 func NewNumber() *Number {
 	r := &Number{
 		Buckets: make(map[int64]*numberBucket),
@@ -25,6 +30,9 @@ func NewNumber() *Number {
 	return r
 }
 
+// getCurrentBucket 获取当前秒的桶，没有则创建一个新的
+//
+// 注意 调用时都在 Mutex 锁代码块中执行
 func (r *Number) getCurrentBucket() *numberBucket {
 	now := time.Now().Unix()
 	var bucket *numberBucket
@@ -38,11 +46,15 @@ func (r *Number) getCurrentBucket() *numberBucket {
 	return bucket
 }
 
+// removeOldBuckets 删除10s前的桶 | 目前仅保留最近10s的桶
+//
+// 注意 调用时都在 Mutex 锁代码块中执行
 func (r *Number) removeOldBuckets() {
 	now := time.Now().Unix() - 10
 
 	for timestamp := range r.Buckets {
 		// TODO: configurable rolling window
+		// TODO: 可配置的滚动窗口
 		if timestamp <= now {
 			delete(r.Buckets, timestamp)
 		}
@@ -51,7 +63,7 @@ func (r *Number) removeOldBuckets() {
 
 // Increment increments the number in current timeBucket.
 //
-// Increment 增加当前 timeBucket 中的数字。
+// Increment 增量统计 | 增加当前 timeBucket 中的数字。
 func (r *Number) Increment(i float64) {
 	if i == 0 {
 		return
@@ -66,6 +78,8 @@ func (r *Number) Increment(i float64) {
 }
 
 // UpdateMax updates the maximum value in the current bucket.
+//
+// UpdateMax 峰值统计 | 更新当前桶中的最大值。| 即：统计在当前秒内最大的值
 func (r *Number) UpdateMax(n float64) {
 	r.Mutex.Lock()
 	defer r.Mutex.Unlock()
@@ -78,6 +92,8 @@ func (r *Number) UpdateMax(n float64) {
 }
 
 // Sum sums the values over the buckets in the last 10 seconds.
+//
+// Sum 查询总和 | 时间段内的指标总和 | 对过去 10 秒内桶中的值求和。
 func (r *Number) Sum(now time.Time) float64 {
 	sum := float64(0)
 
@@ -86,6 +102,7 @@ func (r *Number) Sum(now time.Time) float64 {
 
 	for timestamp, bucket := range r.Buckets {
 		// TODO: configurable rolling window
+		// TODO: 可配置的滚动窗口
 		if timestamp >= now.Unix()-10 {
 			sum += bucket.Value
 		}
@@ -95,6 +112,8 @@ func (r *Number) Sum(now time.Time) float64 {
 }
 
 // Max returns the maximum value seen in the last 10 seconds.
+//
+// Max 查询峰值 | 时间段内的指标最大值 | 返回过去 10 秒内看到的最大值。
 func (r *Number) Max(now time.Time) float64 {
 	var max float64
 
@@ -103,6 +122,7 @@ func (r *Number) Max(now time.Time) float64 {
 
 	for timestamp, bucket := range r.Buckets {
 		// TODO: configurable rolling window
+		// TODO: 可配置的滚动窗口
 		if timestamp >= now.Unix()-10 {
 			if bucket.Value > max {
 				max = bucket.Value
@@ -113,6 +133,7 @@ func (r *Number) Max(now time.Time) float64 {
 	return max
 }
 
+// Avg 查询平均值 | 时间段内的指标平均值 | 返回过去 10 秒内看到的平均值。
 func (r *Number) Avg(now time.Time) float64 {
 	return r.Sum(now) / 10
 }
--- hystrix/rolling/rolling_timing.go
+++ hystrix/rolling/rolling_timing.go
@@ -10,12 +10,17 @@ import (
 // Timing maintains time Durations for each time bucket.
 // The Durations are kept in an array to allow for a variety of
 // statistics to be calculated from the source data.
+//
+// Timing 维护每个时间桶的时间Durations。
+// 持续时间保存在一个数组中，以允许从源数据计算各种统计信息。
 type Timing struct {
 	Buckets map[int64]*timingBucket
 	Mutex   *sync.RWMutex
 
+	// 缓存排序的时间桶 | 对最近60s内的时间桶进行了排序，在这里缓存
 	CachedSortedDurations []time.Duration
-	LastCachedTime        int64
+	// 最新一次缓存排序的时间桶时间 | 比如:获取排序时发现时1秒前排序过，则直接使用缓存的排序结果 CachedSortedDurations
+	LastCachedTime int64
 }
 
 type timingBucket struct {
@@ -23,6 +28,8 @@ type timingBucket struct {
 }
 
 // NewTiming creates a RollingTiming struct.
+//
+// NewTiming 创建一个 RollingTiming 结构。
 func NewTiming() *Timing {
 	r := &Timing{
 		Buckets: make(map[int64]*timingBucket),
@@ -35,10 +42,12 @@ type byDuration []time.Duration
 
 func (c byDuration) Len() int           { return len(c) }
 func (c byDuration) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
-func (c byDuration) Less(i, j int) bool { return c[i] < c[j] }
+func (c byDuration) Less(i, j int) bool { return c[i] < c[j] } // 升序
 
 // SortedDurations returns an array of time.Duration sorted from shortest
 // to longest that have occurred in the last 60 seconds.
+//
+// SortedDurations 返回一个 time.Duration 数组，从最近 60 秒内发生的最短到最长排序。
 func (r *Timing) SortedDurations() []time.Duration {
 	r.Mutex.RLock()
 	t := r.LastCachedTime
@@ -46,6 +55,7 @@ func (r *Timing) SortedDurations() []time.Duration {
 
 	if t+time.Duration(1*time.Second).Nanoseconds() > time.Now().UnixNano() {
 		// don't recalculate if current cache is still fresh
+		// 如果当前缓存仍然是新鲜的，不要重新计算
 		return r.CachedSortedDurations
 	}
 
@@ -72,6 +82,7 @@ func (r *Timing) SortedDurations() []time.Duration {
 	return r.CachedSortedDurations
 }
 
+// getCurrentBucket 获取当前秒的桶，没有则创建一个新的
 func (r *Timing) getCurrentBucket() *timingBucket {
 	r.Mutex.RLock()
 	now := time.Now()
@@ -89,6 +100,9 @@ func (r *Timing) getCurrentBucket() *timingBucket {
 	return bucket
 }
 
+// removeOldBuckets 删除60s前的桶 | 目前仅保留最近10s的桶
+//
+// 注意 调用时都在 Mutex 锁代码块中执行
 func (r *Timing) removeOldBuckets() {
 	now := time.Now()
 
@@ -102,7 +116,7 @@ func (r *Timing) removeOldBuckets() {
 
 // Add appends the time.Duration given to the current time bucket.
 //
-// Add 将 time.Duration 附加到当前时间桶。
+// Add 增量 | 将 time.Duration 附加到当前时间桶。
 func (r *Timing) Add(duration time.Duration) {
 	b := r.getCurrentBucket()
 
@@ -114,6 +128,8 @@ func (r *Timing) Add(duration time.Duration) {
 }
 
 // Percentile computes the percentile given with a linear interpolation.
+//
+// Percentile 计算通过线性插值给出的百分位数。| 返回毫秒ms
 func (r *Timing) Percentile(p float64) uint32 {
 	sortedDurations := r.SortedDurations()
 	length := len(sortedDurations)
@@ -125,15 +141,29 @@ func (r *Timing) Percentile(p float64) uint32 {
 	return uint32(sortedDurations[pos].Nanoseconds() / 1000000)
 }
 
+// ordinal 顺序数 | 计算 percentile(如:50%) 在时间桶[](即:sortedDurations)中的最接近的索引数
+//
+// percentile 百分位数
 func (r *Timing) ordinal(length int, percentile float64) int64 {
 	if percentile == 0 && length > 0 {
 		return 1
 	}
 
+	// Ceil returns the least integer value greater than or equal to x.
+	// Ceil 返回大于或等于 x 的最小整数值。
+	//
+	// Special cases are:
+	// 特殊情况是：
+	//	Ceil(±0) = ±0
+	//	Ceil(±Inf) = ±Inf
+	//	Ceil(NaN) = NaN
+
 	return int64(math.Ceil((percentile / float64(100)) * float64(length)))
 }
 
 // Mean computes the average timing in the last 60 seconds.
+//
+// Mean 查询平均值 | 计算过去 60 秒的平均时间。 | 返回的是毫秒ms
 func (r *Timing) Mean() uint32 {
 	sortedDurations := r.SortedDurations()
 	var sum time.Duration
